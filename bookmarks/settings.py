5eaa8e3fb2e15c2ecc4055074af5edcebf11c0c9
anas <anas.abbal13@gmail.com>
2021-04-21 23:49:43 +0000

    $

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,6 +26,8 @@ SECRET_KEY = 'django-insecure-p0er(^nn-*zjize@q99^t*89srkzm^^c@^(7s(+uk-haze!n07
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+
 
 
 LOGIN_REDIRECT_URL = 'dashboard'
@@ -132,3 +134,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
